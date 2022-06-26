4615988631e3991a86611816bcd435ed8f6129b1
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-06-26 02:48:22 +0200

    Css and js refected

---
+++ resources/assets/website/js/modules.js
@@ -0,0 +1,6 @@
+import './modules/choices';
+import './modules/feather';
+import './modules/tns';
+import  './modules/mask';
+import './modules/easy_background';
+import './modules/aos';
---
+++ resources/assets/website/js/modules/aos.js
@@ -0,0 +1,2 @@
+import Aos from 'aos';
+window.Aos = Aos;
