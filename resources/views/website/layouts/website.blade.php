98e0129412d7b53b84ff24519f5ab16147fad567
Nelson Alexandre Mutane <inkomomutane@github.com>
2023-04-24 17:29:58 +0200

    facebook pixel added to website

--- resources/views/website/layouts/website.blade.php
+++ resources/views/website/layouts/website.blade.php
@@ -18,6 +18,19 @@
        @stack('css')
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @livewireStyles
+    <!-- Meta Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1133822068011030');
+  fbq('track', 'PageView');
+</script>
 </head>
 
 <body>
