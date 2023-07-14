c5a98942cb5a0dea55e97f2f5c730c280c0df5f5
Nelson Alexandre Mutane <inkomomutane@github.com>
2023-07-14 23:51:43 +0200

    null fix

--- app/Actions/Website/Welcome.php
+++ app/Actions/Website/Welcome.php
@@ -18,7 +18,7 @@ class Welcome
         //
         return view('website.welcome', [
             'page' => GetPage::run()->with('media')->first()?->getFirstMedia(Pages::HOME),
-            'thumb' =>  GetPage::run()->with('media')->first()?->getFirstMedia(Pages::HOME)->responsiveImages()->getPlaceholderSvg(),
+            'thumb' =>  GetPage::run()->with('media')->first()?->getFirstMedia(Pages::HOME)?->responsiveImages()?->getPlaceholderSvg(),
             'relevantImovels' => $this->getRelevantImovels(),
             'lastestImovels' => Imovel::with(['bairro.cidade', 'intermediationRule', 'imovelFor', 'tipo_de_imovel', 'status', 'comentarios', 'ratings'])->latest('created_at')->get()->take(6),
             'banners' => Banner::with('media')->first(),
