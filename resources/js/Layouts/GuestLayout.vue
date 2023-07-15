0de5394d280066d44a8217386358e3ff79482cee
Nelson Alexandre Mutane <inkomomutane@github.com>
2023-07-15 21:36:57 +0200

    fix

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -8,7 +8,7 @@ defineProps({
 });
 </script>
 <template>
-    <Header :solid-bg="true"/>
+    <Header v-if="showFooter" :solid-bg="true"/>
     <div class="bg-slate-50 dark:bg-slate-700 selection:bg-primary-500">
         <slot />
     </div>
