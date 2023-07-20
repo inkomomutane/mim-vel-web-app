d23022f040adac37e63bb050ef3d0139952430bc
Nelson Alexandre Mutane <inkomomutane@github.com>
2023-07-20 14:59:35 +0200

    renamed from iframe to google link

--- resources/js/Pages/Imovel/CreateImovel.vue
+++ resources/js/Pages/Imovel/CreateImovel.vue
@@ -386,8 +386,8 @@ const storeImovel = () =>
                     <div>
                         <label
                             for="Mapa"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white capitalize"
-                            >Iframe do imovel no mapa</label
+                            class="block mb-2 text-sm font-medium bg-orange-300 text-gray-900 dark:text-white"
+                            >Link do mapa google (Só para o corretor do imóvel)</label
                         >
                         <textarea
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
