f0eaff4662335ff88ba280dbe4bb411673828f68
Nelson Alexandre Mutane <inkomomutane@github.com>
2023-08-21 15:58:14 +0200

    Super admin added globaly

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
+use Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::before(function (User $user) {
+            return $user->hasRole('Super-Admin') ? true : null;
+        });
     }
 }
