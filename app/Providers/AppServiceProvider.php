4fd5d2dbf7a5dcfa7799821cb66a4765334ed293
inkomomutane <Nelson.mutane@uzambeze.ac.mz>
2025-09-14 07:46:24 +0200

    fix url schemma error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,30 +35,9 @@ class AppServiceProvider extends ServiceProvider
     {
 
 
-       // Determine if HTTPS should be enforced
-        $enforceHttps = $this->app->environment(['production', 'staging'])
-            && !$this->app->runningUnitTests();
- 
-        // Force HTTPS for all generated URLs
-        URL::forceHttps($enforceHttps);
- 
-        // Ensure proper server variable is set
-        if ($enforceHttps) {
-            $this->app['request']->server->set('HTTPS', 'on');
-        }
- 
-        // Set up global middleware for security headers
-        if ($enforceHttps) {
-            $this->app['router']->pushMiddlewareToGroup('web', function ($request, $next){
-                $response = $next($request);
- 
-                return $response->withHeaders([
-                    'Strict-Transport-Security' => 'max-age=31536000; includeSubDomains',
-                    'Content-Security-Policy' => "upgrade-insecure-requests",
-                    'X-Content-Type-Options' => 'nosniff'
-                ]);
-            });
-        }
+        if ($this->app->environment('production')) {
+                URL::forceScheme('https');
+            }
 
         Paginator::useBootstrapFive();
         view()->share([
