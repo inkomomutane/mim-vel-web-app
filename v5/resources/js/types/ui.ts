a3e68d41799d47d22030f69af5ef2cd573a58d19
inkomomutane <Nelson.mutane@uzambeze.ac.mz>
2026-09-17 08:57:04 +0200

    removed

--- v5/resources/js/types/ui.ts
+++
@@ -1,9 +0,0 @@
-export type Appearance = 'light' | 'dark' | 'system';
-export type ResolvedAppearance = 'light' | 'dark';
-
-export type AppVariant = 'header' | 'sidebar';
-
-export type FlashToast = {
-    type: 'success' | 'info' | 'warning' | 'error';
-    message: string;
-};
